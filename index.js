2256cb8c92455346d68359b41c7abf84c93dab54
Eric Storey <estorey@gmail.com>
2019-07-02 18:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
  
-  function onKeyDownHandler(e) {
+   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
    
     if (key === code[index]) {
@@ -18,5 +18,5 @@ function init() {
     } else {
       index = 0;
     }
-  }
+  })
 }
